7d86e2f58e5900c061707806e5885284eebd3cdb
Kristofer Krause <kris.krause@gmail.com>
2012-07-23 21:36:37 -0400

    added upload photo icon

--- src/js/photos.js
+++ src/js/photos.js
@@ -36,6 +36,12 @@ SouthRidge.Views.AlbumView = Backbone.View.extend({
     SouthRidge.Utils.SetTopBar('South Ridge Photos');
     SouthRidge.Utils.ScrollTop();
 
+    forge.topbar.addButton({ icon: "img/167-upload-photo.png", position: "right", tint: [59, 118, 38, 255] }, function () { 
+      forge.file.getImage( { width: 500, height: 500 }, function() {
+
+      });
+    });
+
     $(this.el).empty().show();
 
     var elemt = $('<ul id="mainList" class="list"></ul>');
